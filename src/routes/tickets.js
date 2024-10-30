b5e9334b14fc20c8ad92bd7af9aa561f9488e22c
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-30 10:47:02 -0300

    delete ticket

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -2,9 +2,11 @@ import { create } from "../controllers/tickets/create.js";
 import { index } from "../controllers/tickets/index.js";
 import { update } from "../controllers/tickets/update.js";
 import { updateStatus } from "../controllers/tickets/updateStatus.js";
+import { remove } from "../controllers/tickets/remove.js"
 
 export const tickets = [
     {
+        
         method: "POST",
         path: "/tickets",
         controller: create,
@@ -23,5 +25,10 @@ export const tickets = [
         method: "PATCH",
         path: "/tickets/:id/close",
         controller: updateStatus,
+    },
+    {
+        method: "DELETE",
+        path: "/tickets/:id",
+        controller: remove,
     },
 ]
